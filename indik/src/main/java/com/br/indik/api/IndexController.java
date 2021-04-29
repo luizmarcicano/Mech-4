96d86f64c40f1df13324f87f77929a224ee802d1
luizmarcicano <luiz.marcicano@gmail.com>
2021-04-29 20:27:43 -0300

    Retry

--- indik/src/main/java/com/br/indik/api/IndexController.java
+++ indik/src/main/java/com/br/indik/api/IndexController.java
@@ -10,19 +10,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class IndexController {
-	
-	@GetMapping("/user/{id}")
-	public String getUsuariosById(@PathVariable("id")Long id) {
-		return "Usuário" + id;
+	@RequestMapping("/")
+	public String index(){
+		return "cadastro";
 	}
 	
-	@PostMapping("/cadastro")
-	public String postCadastro(@RequestParam("id") Long id, @RequestParam("nome") String nome) {
-		return "Usuário " + id;
+	@RequestMapping(value = "/cadastrarUsuario", method =RequestMethod.GET)
+	public String cadastro(){
+		return "cadastro";
 	}
-
 	
+	@RequestMapping(value = "/cadastrarUsuario", method =RequestMethod.POST)
+	public String formulario(){
+		return "redirect:/";
+	}
 	//@RequestParam("id")Long id,@RequestParam("nomeCompleto")String nomeCompleto,@RequestParam("cidade")String cidade, @RequestParam("estado")String estado, @RequestParam("dataNascimento")Date dataNascimento, @RequestParam("username")String username, @RequestParam("senha")String senha
 }
