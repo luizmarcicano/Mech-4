d09f155fba527e9bdcead30b478f97a8cb61927c
luizmarcicano_ <luiz.marcicano@gmail.com>
2021-05-07 18:43:28 -0300

    hm

--- src/main/java/com/br/indik/api/IndexController.java
+++ src/main/java/com/br/indik/api/IndexController.java
@@ -1,13 +1,7 @@
 package com.br.indik.api;
 
-import java.sql.Date;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
